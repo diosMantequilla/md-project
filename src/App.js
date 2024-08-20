6bbdf12d1dd3ece239c5e955f775e42330ff72ce
diosMantequilla <yagel.salazar.dev@gmail.com>
2024-08-20 16:15:41 -0600

    ejemplo git

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from './components/Box'
 
 function App() {
+
+  const numeros = [1,2,3,4,5,6]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          numeros.map((item) => <Box></Box>)
+        }
       </header>
     </div>
   );
---
+++ src/components/Box.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './box.css'
+
+const Box = () => {
+    return (
+        <div className='cajita'>
+            <h2>titulo</h2>
+            <p>Este es el contenidoH</p>
+        </div>
+    )
+}
+
+export default Box
